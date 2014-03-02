5f00dbb2273c0f8abb6488ebe099e738aed2c34c
Veli-Matti Luoto <vellu@deveo.com>
2014-03-02 13:29:28 +0200

    Add Deveo colors
    The colors can be used as follows:
    
    Programmatically
      ((TextView) view).setTextColor(ColorUtils.COLOR_RED);
      ((TextView) view).setTextColor(getResources().getColor(R.color.red));
    
    In layout files
      android:textColor="@color/red"

---
+++ android/src/main/java/com/deveo/android/util/ColorUtils.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2014 Deveo Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the “License”);
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an “AS IS” BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.deveo.android.util;
+
+public class ColorUtils {
+
+    public static final int COLOR_BLACK = 0xff000000;
+
+    public static final int COLOR_BLUE = 0xff3589c4;
+
+    public static final int COLOR_DARK = 0xff29587a;
+
+    public static final int COLOR_DARKER = 0xff142630;
+
+    public static final int COLOR_GREY = 0xff8394a0;
+
+    public static final int COLOR_LIGHT = 0xffffffff;
+
+    public static final int COLOR_RED = 0xffffb000;
+
+    public static final int COLOR_GREEN = 0xffc3da09;
+
+}
